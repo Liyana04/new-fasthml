54a105610b9dadf55d1975b0978e28541933949e
Liyana04 <32533357+Liyana04@users.noreply.github.com>
2024-12-29 21:10:23 +0800

    add import sqliteminutils

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 import pytz
 from fasthtml.common import *
+from sqlite_minutils.db import *
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
