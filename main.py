b8d43e67d2d80388ba7280f0800262525417952e
Liyana04 <32533357+Liyana04@users.noreply.github.com>
2024-12-29 20:53:14 +0800

    new 8pm update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 # pytz is python library for timezone
 import pytz
-from supabase import create_client
-from dotenv import load_dotenv
 from fasthtml.common import *
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
@@ -14,8 +14,9 @@ MAX_NAME_CHAR =15
 MAX_MESSAGE_CHAR = 50
 TIMESTAMP_FMT = "%Y-%m-%d %I:%M:%S %p Asia/Singapore"
 
-# initialize supabase client
-supabase = create_client(os.getenv("SUPABASE_URL"),os.getenv("SUPABASE_KEY"))
+# Initialize Supabase client
+supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+
 
 
 # this is a header
